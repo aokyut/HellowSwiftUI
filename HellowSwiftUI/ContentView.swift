c83bb7dea356e77ac9c3fcb38fb2da916ac9a3fa
aokyut <epaperamic@gmail.com>
2020-07-07 23:51:06 +0900

    Initial Commit

---
+++ HellowSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HellowSwiftUI
+//
+//  Created by 青木優太 on 2020/07/07.
+//  Copyright © 2020 okiuta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
